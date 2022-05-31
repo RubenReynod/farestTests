425e9defcaf5c3c9d6be6c3737f634d9fcef9ad1
Ruben2709 <ruben.23292.rr@gmail.com>
2022-05-31 17:43:21 -0500

    Calculo del monto total

--- src/components/Fares.jsx
+++ src/components/Fares.jsx
@@ -1,22 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Fares = ({fares}) => {
-  const [hours, setHours] = useState('');
-  const [updatedFares, setUpdatedFares] = useState(fares);
+  const [arrFares] = useState(fares);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
   const [montoTotal, setMontoTotal] = useState(0);
+  const [statusMonto, setStatusMonto] = useState(0);
 
-  const getMonto = () =>{
-    setMontoTotal( () => updatedFares.reduce( (total, fare) => {
-      const [nBlock, tarifa, minutes, , next] = fare;
-      const m = hours*60;
-      const repeat = hours==0?0:Math.ceil(m / minutes);
-      let t = total + (tarifa * repeat);
-      
-      if(nBlock == next)
-        t += tarifa * repeat;
-       
-      return t;
-    },0))
+  const handleMonto = () => {
+      const fisrt = arrFares[0]
+
+      setMontoTotal(0);
+      getMonto(fisrt, minutes);
+  }
+
+  useEffect(() => {
+    setMinutes(hours*60);
+  },[hours])
+
+  const getMonto = (block, minutes, total=0, end=false) => {
+    const [nBl, tar, min, rep, next] = block;
+    if(minutes <=0 && next != nBl)
+        return;
+    
+    let repeat = Math.ceil(minutes/min)
+        repeat = repeat > rep?rep:repeat;
+    let m = min * repeat;
+    const t = total + Math.ceil(tar * repeat);
+
+    setMontoTotal(t);
+    if(minutes > 0 && !end)
+       getMonto(arrFares[next], (minutes - m), (total + (tar * repeat)), (next == nBl || minutes <= 0)); 
   }  
 
   return(
@@ -26,14 +40,14 @@ const Fares = ({fares}) => {
             type="number"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
             value={hours}
-            onChange={ (e) => setHours(e.target.value) } 
+            onChange={ (e) => setHours(Number(e.target.value)) } 
             />
 
       <p className="text-gray-700 font-bold mb-5">Monto total: <span className="font-normal">$ {montoTotal}</span></p>
       <button 
         type="button"
         className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded"
-        onClick={getMonto}>Obtener Monto
+        onClick={handleMonto}>Obtener Monto
       </button>
     </>
   )
